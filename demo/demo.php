3e20b03530d4179a671d0f89c2a12be34c906dcf
taoran <taoran0796@163.com>
2022-05-05 11:06:22 +0800

    docs

---
+++ demo/demo.php
@@ -0,0 +1,25 @@
+<?php 
+// $bl = 1.07;
+// $base = 36000;
+// $year = 2023;
+// for ($i = 0; $i <= 30; $i++) {
+//   $nowBase = $base * $bl;
+//   echo $year . 'year: ' . $nowBase . PHP_EOL;
+//   $sum += $nowBase;
+//   $base = $nowBase;
+//   $year++;
+// }
+// echo 'sum: ' . $sum . PHP_EOL;
+
+// $bl = 5000;
+// $combo = 40 + 10 + 10 + 
+// $base = 36000;
+// $year = 2023;
+// for ($i = 0; $i <= 30; $i++) {
+//   $nowBase = $base + $bl;
+//   echo $year . 'year: ' . $nowBase . PHP_EOL;
+//   $sum += $nowBase;
+//   $base = $nowBase;
+//   $year++;
+// }
+// echo 'sum: ' . $sum . PHP_EOL;
